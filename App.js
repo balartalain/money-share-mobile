d7c6ac6bc847245c4445c17f0c2cbe5bcfa5a388
Alain Perez Balart <alain.perez@doceleguas.com>
2021-06-03 00:41:58 -0400

    Added header

--- App.js
+++ App.js
@@ -1,17 +1,53 @@
 import React, {useState, useEffect, useRef} from 'react'
 import { SafeAreaView, ScrollView, View, TouchableOpacity, Pressable, Text, StyleSheet, Dimensions, StatusBar } from 'react-native';
+import { Avatar } from "react-native-elements";
 import Constants from 'expo-constants';
-import YearsTabView from './components/YearsTabView';
+import MonthsTabView from './components/MonthsTabView';
 import Menu from './components/Menu';
 
+import FakeData from './components/FakeData';
+
+const getFakeYears = ()=>{
+  return Object.keys(FakeData);  
+}
+const getFakeYearData = (year) =>{
+  return FakeData[year];
+}
+const Header = ()=>{
+  return (
+    <View style={{
+      flex:'1 1' , 
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      backgroundColor:'#3EB489',
+      paddingVertical: 10,
+      paddingHorizontal: 20
+    }}
+    >
+      <Text style={{fontSize:18, color: 'white'}}>Money share</Text>
+      <Avatar
+        rounded
+        overlayContainerStyle={{backgroundColor: 'gray'}}
+        size="medium"
+        title="BP"
+        onPress={() => console.log("Works!")}
+        activeOpacity={0.7}        
+      />
+    </View>
+  )
+}
 const App = () =>{
-  
+  const [index, setIndex] = useState(0);
+  const onSelectedItem = (i) => {
+    setIndex(i);
+    console.log('onSelectedItem '+ i);
+  }
     return (
       <SafeAreaView style={styles.container}>
-        <Menu />
-        {
-          //<YearsTabView /> 
-        }
+        <Header/>
+        <Menu items={getFakeYears()} selectedItem={2} onSelectedItem={onSelectedItem}/>
+        <MonthsTabView  data={getFakeYearData(Object.keys(FakeData)[index]) }  index={index}/>         
       </SafeAreaView>
     );  
 }
