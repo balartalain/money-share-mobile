e6b334466dcc0412760f3cd60b5d4e12bc094aa2
Alain Perez Balart <alain.perez@doceleguas.com>
2021-06-10 20:52:17 -0400

    Added dropdown picker

--- components/AddExpense.js
+++ components/AddExpense.js
@@ -2,13 +2,14 @@ import React, {useState} from "react";
 import { View, StyleSheet, Text, TextInput, TouchableOpacity} from "react-native";
 import { Button } from 'react-native-elements';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import {Picker} from '@react-native-picker/picker';
 import {color, monthNames, dayOfWeek } from '../utils'
 
 
 const AddExpense = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
   const [showDatePicker, setshowDatePicker] = useState(false);
-
+  const [selectedCurrency, setSelectedCurrency] = useState();
   const onChange = (event, selectedDate) => {
     //const currentDate = selectedDate || currentDate;
     setshowDatePicker(Platform.OS === 'ios');
@@ -19,7 +20,7 @@ const AddExpense = () => {
     <View style={{flex:1}}>
       <View style={{flex:1}}>
         <View style={{flex:0, alignItems:'center'}}>
-          <TouchableOpacity style={{paddingVertical:10}} onPress={()=>setshowDatePicker(true)} >
+          <TouchableOpacity style={{paddingVertical:15}} onPress={()=>setshowDatePicker(true)} >
             <Text>{`${dayOfWeek[currentDate.getDay()]}, ${currentDate.getDate()} ${monthNames[currentDate.getMonth()]}`}</Text>
             {showDatePicker && (
               <DateTimePicker style={{backgroundColor: "green"}}
@@ -40,10 +41,30 @@ const AddExpense = () => {
             )}
           </TouchableOpacity>
         </View>
-        <TextInput style={styles.field} placeholder='Cantidad' />
+        <TextInput style={[styles.field, {
+                    borderTopWidth: 1,
+                    borderTopColor: 'rgb(216, 216, 216)'  
+        }]} placeholder='Cantidad' />
         <TextInput style={styles.field} placeholder='Concepto' />
         <TextInput style={styles.field} placeholder='Comentario' />
-        <TextInput style={styles.field} placeholder='Gasto o Ingreso' />            
+        <View style={{
+          paddingVertical: 5,
+          paddingLeft: 10,
+          borderWidth: 0,
+          borderBottomWidth: 1,
+          borderBottomColor: 'rgb(216, 216, 216)'  
+        }}>
+          <Picker 
+            dropdownIconColor={color.primaryGreen}
+            selectedValue={selectedCurrency}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedCurrency(itemValue)
+            }>
+              {/* // https://github.com/react-native-picker/picker */} 
+            <Picker.Item label="USD" value="usd" />
+            <Picker.Item label="CUP" value="cup" />
+          </Picker> 
+        </View>
       </View>
       <View style={{flex:0}}>
       <Button title="Aceptar" 
