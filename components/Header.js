44d3b9c9d9dae7bcacfe9df92fd801c8e861adc1
Alain Perez Balart <alain.perez@doceleguas.com>
2021-08-13 16:19:04 -0400

    Added unmount animation

--- components/Header.js
+++ components/Header.js
@@ -1,67 +1,111 @@
-import React, {useContext, useEffect, useState} from 'react'
-import { View, Text, TouchableOpacity, Dimensions} from 'react-native'
+import React, {useContext, useEffect, useState, useRef} from 'react'
+import { View, Text, TouchableOpacity, Dimensions, Animated} from 'react-native'
 import PropTypes from 'prop-types'
 import Ripple from 'react-native-material-ripple'
 import { AntDesign } from '@expo/vector-icons' 
 import { UserDataContext, useUserDataContextHook } from './UserDataContext'
 import { color } from './../utils'
 import { getEnvironment } from '../utils'
-import { View as MotiView, AnimatePresence } from 'moti'
-
+import FadeInView from './FadeInView'
+import { useAnimation } from '../hooks/useAnimation'
 
 const { width } = Dimensions.get('window')
 const ENV = getEnvironment().envName
 
-const Delete = () =>{
+const Delete = (props) =>{
     const {deleteItems, markedItemsToDelete, setMarkedItemsToDelete} = useUserDataContextHook()
-    const [color, setColor] = useState('white')
+    const [trashColor, setTrashColor] = useState('white')
+    const [shouldBeRender, setShouldBeRender] = useState(false)
+    const isVisible = useRef(false)
+    const animation = useRef(new Animated.Value(0)).current
+
+    // useEffect(() => {
+    //     animation.addListener((value) => {
+    //         console.log(value)
+    //     })
     
+    //     return () => {
+    //         animation.removeAllListeners()
+    //     }
+    // }, [])
+
+    useEffect(() => {
+        if (!props.visible && isVisible.current ){
+            animation.setValue(1)
+            Animated.timing(animation, {
+                toValue: 0,
+                duration: 300,
+                useNativeDriver: true
+            }).start(()=>{
+                isVisible.current = false
+                setShouldBeRender(false)
+            })
+            
+        } else  
+        if (props.visible && !isVisible.current ){  
+            setShouldBeRender(true)                                
+            animation.setValue(0)
+            Animated.timing(animation, {
+                toValue: 1,
+                duration: 500,
+                useNativeDriver: true
+            }).start((finished)=>{
+                console.log(finished)
+                isVisible.current = true
+            })
+        } else  
+        if (!props.visible && !isVisible.current ){
+            setShouldBeRender(false)
+        }
+
+    }, [props.visible])
+
     useEffect(() => {
         if (markedItemsToDelete.length > 1){
-            setColor('orange')
-            setTimeout(()=>{setColor('white')}, 200)
+            setTrashColor('orange')
+            setTimeout(()=>{setTrashColor('white')}, 200)
         }
     }, [markedItemsToDelete.length])
-
+    const opacityStyle = animation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 1]
+    })
+    const transform = [{
+        scale: animation.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0.3, 1]
+        })
+    }]
     return (
-        <MotiView
-            from={{
-                opacity: 0,
-                scale: 0.9,
-            }}
-            animate={{
-                opacity: 1,
-                scale: 1,
-            }}
-            exit={{
-                opacity: 0,
-                scale: 0.9,
-            }} 
-            transition={{
-                type: 'timing',
-                duration: 100
-            }}
-            style={{
-                flex:1,
-                flexDirection: 'row',   
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                paddingHorizontal: 10,            
-                top:0, 
-                left: 0,           
-                height: 65,        
-                width: width,
-                position: 'absolute',
-                zIndex: 10,
-                backgroundColor: color.primaryGreen        
-            }}>
-            <Ripple onPress={()=>setMarkedItemsToDelete([])} >
-                <AntDesign name="arrowleft" size={24} color='white' />
-            </Ripple>
-            <Ripple onPress={()=>deleteItems()} >
-                <AntDesign name="delete" size={24} color={color} />
-            </Ripple>
-        </MotiView>
+        <> 
+            { shouldBeRender &&
+                <Animated.View            
+                    style={{
+                        flex:1,
+                        flexDirection: 'row',   
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        paddingHorizontal: 10,            
+                        top:0, 
+                        left: 0,           
+                        height: 65,        
+                        width: width,
+                        position: 'absolute',
+                        zIndex: 1,
+                        backgroundColor: color.primaryGreen,
+                        opacity: opacityStyle,
+                        transform: transform
+                               
+                    }}>
+                    <Ripple onPress={()=>setMarkedItemsToDelete([])} >
+                        <AntDesign name="arrowleft" size={24} color='white' />
+                    </Ripple>
+                    <Ripple onPress={()=>deleteItems()} >
+                        <AntDesign name="delete" size={24} color={trashColor} />
+                    </Ripple>
+                </Animated.View>
+            }
+        </>
     )}
 const Header = (props)=>{  
     const {currentUser, markedItemsToDelete} = useContext(UserDataContext)
@@ -73,8 +117,7 @@ const Header = (props)=>{
             height:65
             
         }}>  
-            <AnimatePresence>{  markedItemsToDelete.length > 0 && <Delete /> }</AnimatePresence>
-            
+            <Delete visible={ markedItemsToDelete.length > 0} />
             <View style={{
                 flex:1,
                 flexDirection: 'row',   
@@ -94,4 +137,4 @@ Header.propTypes = {
     navigation: PropTypes.object.isRequired,
     currentUser: PropTypes.object.isRequired
 }
-export default Header
+export default React.memo(Header)
